655dcf3c477fa44e40a667cf0e27ba17555d7885
Raul Val <raulval@outlook.com>
2023-01-23 16:26:14 -0300

    🔧 update truffle for hdwallet

--- smartcontract/truffle-config.js
+++ smartcontract/truffle-config.js
@@ -1,9 +1,27 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const privateKey = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 7545,
       network_id: "*",
+    },
+    matic: {
+      provider: () =>
+        new HDWalletProvider({
+          privateKeys: [privateKey],
+          providerOrUrl: `https://rpc-mumbai.maticvigil.com/`,
+          chainId: 80001,
+        }),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      chainId: 80001,
     },
   },
   contracts_directory: "./contracts",
@@ -16,7 +34,6 @@ module.exports = {
       },
     },
   },
-
   db: {
     enabled: false,
   },
